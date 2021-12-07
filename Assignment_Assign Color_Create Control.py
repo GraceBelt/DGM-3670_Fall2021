3a5d8e46d0f35b61a21fc95f84cdd03b0044f76e
Grace <heya21st@gmail.com>
2021-12-07 07:32:59 -0700

    Assignment_Assign Color_Create Control.py

--- Assignment_Assign Color_Create Control.py
+++ Assignment_Assign Color_Create Control.py
@@ -1,42 +1,47 @@
 import maya.cmds as cmds
 
-def createControls (num1, num2):
+#2) Select Objects, then select this statement, then Ctrl + Enter
+sels = cmds.ls(selection=True)
+
+for sel in sels:
+    print(sel+'.translate')
+    position = cmds.getAttr(sel+'.translate')
+    pos = sel+"_Ctrl"
+    print(position)
+    cmds.circle(c=position[0], n=pos)
+
+#1) Select this statement, then Ctrl + Enter
+def createObj (num1):
     a = num1
-    b = num2
     
-    for i in range(a, b):
-        newControl = cmds.CreateNURBSCircle()
-              
-createControls(0, 3)
-
-def newGroups (num1, num2):
-    a = num1
-    b = num2
-    c = 'nurbsCircle1'
-    d = 'nurbsCircle2'
-    e = 'nurbsCircle3'
-    f = 'Elbow_Ctrl_Grp'
-    h = 'Wrist_Ctrl_Grp'
+    for i in range(a):
+        cmds.polyCube()
      
-    for i in range(a, b):
-        G1 = cmds.group(c, n='Shoulder_Ctrl_Grp')
-        G2 = cmds.group(d, n='Elbow_Ctrl_Grp')
-        G3 = cmds.group(e, n='Wrist_Ctrl_Grp')
-        Re1 = cmds.rename(c, 'Shoulder_Ctrl')
-        Re2 = cmds.rename(d, 'Elbow_Ctrl')
-        Re3 = cmds.rename(e, 'Wrist_Ctrl')
-        Pa1 = cmds.parent (f, 'Shoulder_Ctrl')
-        Pa2 = cmds.parent(h, 'Elbow_Ctrl')
-           
-newGroups (0, 1)
+createObj(3)
 
-def Colors (red, blue, yellow):
-    newGroups()
-    x = red
-    y = blue
-    z = yellow
+#3) Select whole, then select this statement, then Ctrl + Enter
+sels = cmds.ls(selection=True)
+a = ['pCube1', 'pCube2', 'pCube3']
+b = ['Shoulder_Geo', 'Elbow_Geo', 'Wrist_Geo']
+c = ['pCube1_Ctrl', 'pCube2_Ctrl', 'pCube3_Ctrl']
+d = ['Shoulder_Ctrl', 'Elbow_Ctrl', 'Wrist_Ctrl']
+Group1 = ['Shoulder_Geo', 'Shoulder_Ctrl']
+Group2 = ['Elbow_Geo', 'Elbow_Ctrl']
+Group3 = ['Wrist_Geo', 'Wrist_Ctrl']
 
-    for i in range(x, y, z):
-        
-
-Colors(0, 1, 2) 
+for sel in sels:
+    cmds.rename(a[0], b[0])
+    cmds.rename(a[1], b[1])
+    cmds.rename(a[2], b[2])
+    cmds.rename(c[0], d[0])
+    cmds.rename(c[1], d[1])
+    cmds.rename(c[2], d[2])
+    cmds.group(Group1, n="Shoulder_Ctrl_Grp")
+    cmds.group(Group2, n="Elbow_Ctrl_Grp")
+    cmds.group(Group3, n="Wrist_Ctrl_Grp")
+    cmds.parent('Shoulder_Ctrl', 'Shoulder_Geo')
+    cmds.parent('Elbow_Ctrl', 'Elbow_Geo')
+    cmds.parent('Wrist_Ctrl', 'Wrist_Geo')
+    cmds.move(0, 0, -1.527, 'Shoulder_Geo', absolute=True) 
+    cmds.move(-2.493, 0, 0.459, 'Elbow_Geo', absolute=True) 
+    cmds.move(0, 0, 2.483, 'Wrist_Geo', absolute=True) 
